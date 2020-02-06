1f84f24f44c09c8169e7c05b8ebe8181c4cb37aa
Shubham Singh <singh.shubham987@gmail.com>
2020-02-06 23:10:13 +0530

    commit #2

--- opticalflow.py
+++ opticalflow.py
@@ -1,24 +1,33 @@
+import cv2
 import numpy as np
-import cv2 as cv
-cap = cv.VideoCapture(0)
-ret, frame1 = cap.read()
-prvs = cv.cvtColor(frame1,cv.COLOR_BGR2GRAY)
-hsv = np.zeros_like(frame1)
-hsv[...,1] = 255
+
+capture = cv2.VideoCapture(0)
+
+# History, Threshold, DetectShadows 
+# fgbg = cv2.createBackgroundSubtractorMOG2(50, 200, True)
+fgbg = cv2.createBackgroundSubtractorMOG2(300, 400, True)
+
+# Keeps track of what frame we're on
+frameCount = 0
+
 while(1):
-    ret, frame2 = cap.read()
-    next = cv.cvtColor(frame2,cv.COLOR_BGR2GRAY)
-    flow = cv.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-    mag, ang = cv.cartToPolar(flow[...,0], flow[...,1])
-    hsv[...,0] = ang*180/np.pi/2
-    hsv[...,2] = cv.normalize(mag,None,0,255,cv.NORM_MINMAX)
-    bgr = cv.cvtColor(hsv,cv.COLOR_HSV2BGR)
-    cv.imshow('frame2',bgr)
-    k = cv.waitKey(30) & 0xff == 'q'
-        break
-    else:
-        cv.imwrite('opticalfb.png',frame2)
-        cv.imwrite('opticalhsv.png',bgr)
-    prvs = next
-cap.release()
-cv.destroyAllWindows()
+
+	ret, frame = capture.read() # capture every frame
+	frameCount += 1   # count frame elasped
+	resizedFrame = cv2.resize(frame, (0, 0), fx=0.50, fy=0.50) # scale down the frame to increase perfomance
+	fgmask = fgbg.apply(resizedFrame) #apply 
+	count = np.count_nonzero(fgmask)  # count pixel changes
+
+	if (frameCount > 10 and count > 600): # tune the sensitivity 
+		cv2.putText(resizedFrame, 'cheating', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+	cv2.imshow('Frame', resizedFrame) # actual footage
+	cv2.imshow('Mask', fgmask)     # pixel changes
+
+
+	k = cv2.waitKey(1) & 0xff #press esc to quit
+	if k == 27:
+		break
+
+capture.release()
+cv2.destroyAllWindows()
